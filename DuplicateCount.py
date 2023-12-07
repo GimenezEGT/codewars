bbad1d1efb51ffa6f9a31ffb5629fd7a32c445dd
GimenezEGT <gimenezenrico@yahoo.com.br>
2023-12-07 10:26:29 -0300

    DuplicateCount

---
+++ DuplicateCount.py
@@ -0,0 +1,11 @@
+def duplicate_count(text):
+    string = text.lower()
+    uniqueLetters = ''.join(set(string))
+    counter2 = 0
+    for letter in uniqueLetters:
+        counter =  string.count(letter)
+        if counter > 1:
+            counter2 += 1
+    return counter2
+
+print(duplicate_count("abcdeaB"))
